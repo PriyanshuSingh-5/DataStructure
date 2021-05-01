394d52a4e225ac16afd11148ab41b28c7c4c02b0
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-05-01 16:48:41 +0530

    [Priyanshu]added linked list

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -11,6 +11,10 @@ namespace DataStructure
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to data structure");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
         }
     }
 }
