d17c645191dc7c895f6cb950ee448f4944ed0fe7
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-05-02 09:56:31 +0530

    [Priyanshu]modified insertion at front

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -12,9 +12,16 @@ namespace DataStructure
         {
             Console.WriteLine("Welcome to data structure");
             LinkedList list = new LinkedList();
-            list.InsertLast(56);
-            list.InsertLast(30);
-            list.InsertLast(70);
+            //list.InsertLast(56);
+            //list.InsertLast(30);
+            //list.InsertLast(70);
+
+            list.InsertFront(70);
+            list.InsertFront(30);
+            list.InsertFront(56);
+
+
+           
             list.Display();
             Console.ReadLine();
 
